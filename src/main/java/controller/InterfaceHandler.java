df4281e53572dd5aa89aa200134b65ea967dd89d
evelyn-chan <evelyn@vcg.berkeley.edu>
2017-07-29 13:49:41 -0700

    directory finalized '

--- src/main/java/controller/InterfaceHandler.java
+++ src/main/java/controller/InterfaceHandler.java
@@ -3,6 +3,7 @@ package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import util.Application;
 
 /**
  * Created by KunalDesai on 7/29/17.
@@ -12,9 +13,15 @@ public class InterfaceHandler {
 
     @RequestMapping("/room")
     public ModelAndView getRoom() {
-        System.out.println("Accessed room...");
+        Application.logger.debug("Accessed room...");
         ModelAndView room = new ModelAndView("index");
         return room;
+    }
+
+    @RequestMapping("/room/instructor")
+    public ModelAndView getInstructorView() {
+        Application.logger.debug("Accessed instructor view...");
+        return new ModelAndView("instructor");
     }
 
 }
