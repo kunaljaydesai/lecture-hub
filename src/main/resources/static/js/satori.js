3765c3b4aaed8ccc6a1f9f8dbe62bc904fc8e041
VINCENT CHIU <vcentchiu@gmail.com>
2017-07-29 19:14:26 -0700

    works

---
+++ src/main/resources/static/js/satori.js
@@ -0,0 +1,34 @@
+$(function() {
+    var endpoint = "wss://x5xqvzll.api.satori.com";
+    var appkey = "554F2EC95da36B82cD2fa7E6ddE442fE";
+    var channelName = $("#info").data("channel");
+    console.log(channelName);
+    var authProvider;
+
+    var client = new RTM(endpoint, appkey);
+
+    client.on("enter-connected", function () {
+        addPost("Connected to Satori RTM!");
+        console.log('Connected to Satori RTM!');
+    });
+
+    var subscription = client.subscribe(channelName, RTM.SubscriptionMode.SIMPLE);
+
+    subscription.on('rtm/subscription/data', function (pdu) {
+        pdu.body.messages.forEach(function (msg) {
+            console.log('Got message:', msg);
+        });
+    });
+
+
+
+    client.start();
+
+});
+
+function addPost(text) {
+   var post = "<div>" + text + "</div>";
+   console.log(post);
+   $("#discussion-content").append(post);
+}
+
