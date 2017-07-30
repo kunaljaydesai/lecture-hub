3e965e418ce109cb9c59f9fbd3bcbace3fdbf938
VINCENT CHIU <vcentchiu@gmail.com>
2017-07-29 17:30:42 -0700

    test

---
+++ target/classes/static/js/main.js
@@ -0,0 +1,14 @@
+var COLORS = {
+    orange: "#FA8500",
+    lightBlue: "#2B92FF",
+    darkBlue: "#031D4A",
+    offWhite: "#F0F3F4",
+    gray: "#D0D3D4"
+}
+
+
+var $chat = $("#chat-input");
+
+$("#chat-input").keyup(function(e) {
+	console.log("hello");
+});
